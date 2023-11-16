86d47cf9707691939a3d287944591a84877d094a
Vinicius Medeiros <vini9797@hotmail.com>
2023-11-15 22:06:33 -0300

    Add compatibility 17 + app properties

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,8 +1,21 @@
 package br.com.fiap;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main {
+
+    @RequestMapping("/")
+    public String home() {
+        return "API - FollowCare";
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args);
     }
 }
